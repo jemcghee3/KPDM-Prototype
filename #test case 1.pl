7deb551e828bd609ae196264729f52c1beac45cf
James McGhee <59738821+jemcghee3@users.noreply.github.com>
2023-06-10 14:14:42 +0200

    additional rules

--- #test case 1.pl
+++ #test case 1.pl
@@ -9,16 +9,34 @@ estimate_pd_claim(01, 16000000).
 estimate_bi_claim(01, 3000000).
 estimate_ee_claim(01, 25000000).
 business_interruption_duration(01, 18).
+covers_loss(10101, 01).
+
+cause_of_loss(02, earthquake).
+repair_plan(02, none).
+estimate_pd_claim(02, 16000000).
+estimate_bi_claim(02, 3000000).
+estimate_ee_claim(02, 25000000).
+business_interruption_duration(02, 18).
+covers_loss(10101, 02).
+
+cause_of_loss(03, war).
+repair_plan(03, none).
+estimate_pd_claim(03, 16000000).
+estimate_bi_claim(03, 3000000).
+estimate_ee_claim(03, 25000000).
+business_interruption_duration(03, 18).
+covers_loss(10101, 03).
+
 
 % policy related facts
 applicable_policy(10101, 101).
-covers_loss(10101, 01).
+
 policy_type(10101, iar).
 pd_deductible(10101, 500000).
 bi_deductible(10101, 30).
 pd_bi_limit(10101, 250000000).
 ee_limit(10101, 25000000).
-exclusion(10101, war).
+sublimit(10101, war, 0). % could also call this an exclusion
 
 % slip related facts
 sublimit(101, bi, 2000000). 
@@ -116,7 +134,7 @@ Net_Claim is (NumberOfMonths - (BI_Waiting_Period / 30)) * EstimatedBIClaim.
 
 
 % Rule 14: It is possible for a Policy to exclude one or more Exclusions.
-exclusion(PolicyID, Cause).
+exclusion(PolicyID, Cause) :- sublimit(PolicyID, Cause, 0).
 
 % Rule 15: It is possible for two Exclusions of the same Slip to be different from each other.
 % Rule 14 takes care of this.
@@ -163,78 +181,115 @@ provides_coverage(SlipID, ee) :- provides_coverage(PolicyID, ee), applicable_pol
 provides_ee_coverage(PolicyId) :- provides_bi_coverage(PolicyId).
 
 
-% Rule 25: It is permitted for a Policy to be of any type to give Coverage for BI.\
-% provides_bi_coverage(PolicyId) :- policy(PolicyId).\
+% Rule 25: It is permitted for a Policy to be of any type to give Coverage for BI.
+% provides_bi_coverage(PolicyId) :- policy(PolicyId).
 % Rule 12 handles this?
 
 % Rule 26: It is necessary for each Slip to share a Share of the Policy.
-share(SlipID, Percent), applicable_policy(PolicyID, SlipID).
+isShare(SlipID, PolicyID) :- share(SlipID, Percent), applicable_policy(PolicyID, SlipID).
+% do we need to relate this directly to a slip? I do not think so, but this could be a useful shortcut
 
-% need a loss share number to calculate Acme's share.
+% Rule 27: It is obligatory for the Deductible to be either a PD-Deductible or a BI-Deductible.
+% deductible_type(DeductibleId, Type) :- deductible(DeductibleId), member(Type, [pd, bi]).
+pd_deductible(PolicyID, Amount).
+bi_deductible(PolicyID, Days).
 
-% Rule 27: It is obligatory for the Deductible to be either a PD-Deductible or a BI-Deductible.\
-deductible_type(DeductibleId, Type) :- deductible(DeductibleId), member(Type, [pd, bi]).\
-\
-% Rule 28: It is obligatory for the BI-Deductible to be formatted an integer.\
-deductible_format(DeductibleId) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(DeductibleId).\
-\
-% Rule 29: It is obligatory for BI-Deductible to be expressed in number of days.\
-bi_deductible_days(DeductibleId, Days) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(Days).\
-\
-% Rule 30: It is possible for the BI-Deductible to be first 15 days of the Loss.\
-bi_deductible_days(DeductibleId, 15).\
-\
+% Rule 28: It is obligatory for the BI-Deductible to be formatted an integer.
+% deductible_format(DeductibleId) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(DeductibleId).
+% leave this out
+
+% Rule 29: It is obligatory for BI-Deductible to be expressed in number of days.
+% bi_deductible_days(DeductibleId, Days) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(Days).
+% leave this out
+
+
+% Rule 30: It is possible for the BI-Deductible to be first 15 days of the Loss.
+% bi_deductible_days(DeductibleId, 15).
+% remove
+
 % Rule 31: It is possible for the BI-Deductible to be first 30 days of the Loss.
-bi_deductible_days(DeductibleId, 30).
+% bi_deductible_days(DeductibleId, 30).
+% remove
 
 % Rule 33: It is possible for the BI-Deductible to be 15 times the average daily Loss.
 bi_deductible_amount(DeductibleId, DeductibleAmount) :- bi_deductible_days(DeductibleId, Days), Days is 15 * Average, average_daily_loss(Average).
+% does this work? We can also remove
 
 % Rule 34: It is obligatory for an Insured to pay a Deductible before the Insurer pays the Claim.
 % We should add a "deductible_pay_date" in the definition of a Deductible? And then say Deductible_pay_date > Claim_pay_date.
+% I think we can check if the claim is greater than the deductible
+deductibleSatisfied(LossID) :- estimate_pd_claim(LossID, Amount),
+covers_loss(PolicyID, LossID),
+pd_deductible(PolicyID, Deductible),
+Amount > Deductible.
+
+deductibleSatisfied(LossID) :- estimate_bi_claim(LossID, Amount),
+covers_loss(PolicyID, LossID),
+bi_deductible(PolicyID, Deductible),
+Amount > Deductible.
+
 
 % Rule 35: It is permitted for a Deductible to be a Per-Claim Deductible.
-deductible_type(DeductibleId, Per_Claim_Deductible).
+% deductible_type(DeductibleId, Per_Claim_Deductible).
 
 % Rule 36: It is forbidden for the Insurer to grant Coverage for any Claim lower than the Per-Claim Deductible.
 % Rule 37: It is obligatory for the Insurer to offer Coverage for any Claim higher than the Per-Claim Deductible.
 % This can be translated to: the Claim Coverage must be higher than the Per-Claim Deductible.
-claim_after_deductible(LossID, Net_Claim, bi) :- bi_deductible_amount(DeductibleId, Per_Claim_Deductible_Amount), Net_Claim > Per_Claim_Deductible_Amount. 
+% claim_after_deductible(LossID, Net_Claim, bi) :- bi_deductible_amount(DeductibleId, Per_Claim_Deductible_Amount), Net_Claim > Per_Claim_Deductible_Amount. 
 
 % Rule 38: It is permitted for a Deductible to be an Aggregate Deductible.
-deductible_type(DeductibleId, Aggregate_Deductible).
+% deductible_type(DeductibleId, Aggregate_Deductible).
 
 % Rule 39: It is obligatory for the Insurer to provide Coverage for a Claim if and only if the Deductible is Satisfied.
-coverage_claim(InsurerId, ClaimId) :- claim(ClaimId), deductible_satisfied(DeductibleId). % not sure about this one
+% coverage_claim(InsurerId, ClaimId) :- claim(ClaimId), deductible_satisfied(DeductibleId). % not sure about this one
+% Rule 34 handles this, as well as Rule 13.
+
 
 % Rule 40: A Per-Claim Deductible is Satisfied if the Claim is for a higher amount than the Per-Claim Deductible.
 % I believe Rules 36 and 37 already satisfy this condition. Or we can do something like:
-per_claim_deductible_deductible_satisied(DeductibleId) :- deductible_type(DeductibleId, Per_Claim_Deductible), 
-bi_deductible_amount(DeductibleId, Per_Claim_Deductible_Amount), 
-claim_after_deductible(LossID, Net_Claim, bi), 
-Net_Claim > Per_Claim_Deductible_Amount. 
+% per_claim_deductible_deductible_satisied(DeductibleId) :- deductible_type(DeductibleId, Per_Claim_Deductible), 
+% bi_deductible_amount(DeductibleId, Per_Claim_Deductible_Amount), 
+% claim_after_deductible(LossID, Net_Claim, bi), 
+% Net_Claim > Per_Claim_Deductible_Amount. 
 
 % Rule 41: An Aggregate Deductible is Satisfied if the sum of the amounts of all Claims on the Policy is for a higher amount than the Aggregate Deductible.
 % Not sure how to do a sum. Found this on StackOverflow:
 % list_sum_claims([],0).
 % list_sum_claims([Head|Tail], TotalSum) :- list_sum_claims(Tail, Sum1), TotalSum is Head+Sum1.
-aggregate_deductible_satisied(DeductibleId) :- Net_Claim_Sum(Net_Claim), bi_deductible_amount(DeductibleId, Aggregate_Deductible_Amount), Net_Claim_Sum > Aggregate_Deductible_Amount.
+% aggregate_deductible_satisied(DeductibleId) :- Net_Claim_Sum(Net_Claim), bi_deductible_amount(DeductibleId, Aggregate_Deductible_Amount), Net_Claim_Sum > Aggregate_Deductible_Amount.
 
 % Rule 42: It is necessary for an Insured to file one or more Claims.
-file_claim(InsurerId, ClaimId) :- insurer(InsurerId), claim(ClaimId).
+% file_claim(InsurerId, ClaimId) :- insurer(InsurerId), claim(ClaimId).
 
 % Rule 43: It is possible for a Claim to be covered by Coverage of one or more Policies.
-coverage_claim(ClaimId, PolicyId) :- claim(ClaimId), policy(PolicyId).
+% coverage_claim(ClaimId, PolicyId) :- claim(ClaimId), policy(PolicyId).
+% Rule 1.1 already covers this use case.
 
 % Rule 44: It is necessary for a Loss to present as an event.
 eventId(LossId).
 
 % Rule 45: It is obligatory for a Claim to be examined by one or more Experts.
 % Rule 46: It is obligatory that an Expert examines the Claim.
-examines_claim(ExpertId, ClaimId) :- expert(ExpertId), claim(ClaimId).
+% examines_claim(ExpertId, ClaimId) :- expert(ExpertId), claim(ClaimId).
+% the above says if there is a claim and there is an expert, then the expert examines the claim. But the expert may examine a different claim.
 
-% Rule 47: It is permitted for the Coverage to be limited by a Limit due to the Cause of Loss.  
- 
+
+% Rule 47: It is permitted for the Coverage to be limited by a Limit due to the Cause of Loss.
+
+coverage_limit(LossID, Sublimit) :- covers_loss(PolicyID, LossID),
+applicable_policy(PolicyID, SlipID),
+pd_bi_limit(PolicyID, Amount),
+cause_of_loss(LossID, Cause),
+sublimit(SlipID, Cause, Sublimit),
+nonvar(Sublimit),
+Sublimit < Amount.
+
+coverage_limit(LossID, Amount) :- covers_loss(PolicyID, LossID),
+pd_bi_limit(PolicyID, Amount).
+
+
+
 
 % Rule 48: It is permitted for the Slip to provide no Coverage for a Claim due to the Cause of Loss.  
 % Can we say that the net_claim has to be zero?
+% Yes, I think so. Just limit the sublimit amount to zero. Then Rule 47 takes over.
