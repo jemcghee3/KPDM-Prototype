f8d00668c00fd973fea18fef49eb7da227e5d76a
James McGhee <59738821+jemcghee3@users.noreply.github.com>
2023-06-10 06:29:21 +0200

    additional rules added

--- #test case 1.pl
+++ #test case 1.pl
@@ -1,20 +1,31 @@
 loss(01).
+policy(10101).
+slip(101).
+
+% loss related facts
 cause_of_loss(01, fire).
 repair_plan(01, none).
 estimate_pd_claim(01, 16000000).
 estimate_bi_claim(01, 3000000).
 estimate_ee_claim(01, 25000000).
 business_interruption_duration(01, 18).
-covers_loss(101, 01).
-policy_type(101, iar).
-pd_deductible(101, 500000).
-bi_deductible(101, 30).
-pd_bi_limit(101, 250000000).
-ee_limit(101, 25000000).
-sublimit_for_cause(101, earthquake, 50000000).
+
+% policy related facts
+applicable_policy(10101, 101).
+covers_loss(10101, 01).
+policy_type(10101, iar).
+pd_deductible(10101, 500000).
+bi_deductible(10101, 30).
+pd_bi_limit(10101, 250000000).
+ee_limit(10101, 25000000).
+exclusion(10101, war).
+
+% slip related facts
+sublimit(101, bi, 2000000). 
+sublimit(101, earthquake, 50000000).
 acme_insures_share(101, 0.07).
-acme_slip_differs(101, true).
-difference_adverse_impact(01, true).
+% acme_slip_differs(101, true). %this needs to be a rule
+% difference_adverse_impact(101, 01, true). % this needs to be a rule
 other_policies_covering_loss(01, true).
 
 
@@ -23,24 +34,27 @@ other_policies_covering_loss(01, true).
 
 
 % Rule 1: It is necessary that an Insured purchases one or more Policies.
-% applicable_policy(InsuredID, PolicyID). % important is to relate the insured to the policy, but I think better is to relate the loss to the policy.
+applicable_policy(PolicyID, SlipID). % important is to relate the insured to the policy, but I think better is to relate the loss to the policy.
 
 % Rule 1.1: It is necessary for a Loss to relate to a Policy.
-% covers_loss(PolicyID, LossID). % I think this is a fact, not a rule
+covers_loss(PolicyID, LossID). % I think this is a fact, not a rule
 
 % Rule 2: It is necessary that a Policy insure one or more Insured.
 % maybe remove this rule? It is formalized in Rule 1, isnt it? We can simplify the one or more insureds since this has to do with subsidiary companies or subcontractors and not core for the project
 
 % Rule 3: It is possible that a Policy is an aggregate one or more Slips.
-aggregate_policy(PolicyID, SlipID).
+% aggregate_policy(PolicyID, SlipID).
+% Rule 1 already covers this
 
 
 % Rule 4: It is necessary that Slip is written by one Insurer.
-writes_slip(InsurerID, SlipID). % need to connect insurer and slip
+% writes_slip(InsurerID, SlipID). % need to connect insurer and slip
+% Rule 1 sufficient
 
 % Rule 4.1: It is necessary that an Insurer that writes a slip on a policy insure the Insured.
 % insure(InsurerID, PolicyID, InsuredID) :- writes_slip(InsurerID, SlipID), aggregate_policy(PolicyID, SlipID), applicable_policy(InsuredID, PolicyID).
 % I dont think it matters? Too much informtion about non-Acme insurers
+% could write a rule to translate a slip->policy relationship into a slip-> insured or -> loss, but necessary?
 
 % Rule 5: It is necessary that an Insurer writes a Slip.
 % insurer_writes_slip(SlipId, InsurerId) :- slip(SlipId), insurer(InsurerId).
@@ -48,7 +62,10 @@ writes_slip(InsurerID, SlipID). % need to connect insurer and slip
 
 % Rule 6: It is possible for a Slip to contain one or more Endorsements.
 % slip_contains_endorsements(SlipId, EndorsementIds) :- slip(SlipId), findall(EndorsementId, endorsement(EndorsementId), EndorsementIds).
-% We dont actually use endorsements outside of this rule.
+% We dont actually use endorsements outside of this rule. Maybe say that the slip can be different from the policy.
+
+
+
 
 % Rule 7: It is necessary for one or more Insurer to belong to a Market.
 % insurer_belongs_to_market(InsurerId, MarketId) :- insurer(InsurerId), market(MarketId).
@@ -96,3 +113,71 @@ estimate_bi_claim(LossID, EstimatedBIClaim),
 bi_deductible(PolicyID, BI_Waiting_Period),
 business_interruption_duration(LossID, NumberOfMonths),
 Net_Claim is (NumberOfMonths - (BI_Waiting_Period / 30)) * EstimatedBIClaim.
+
+
+% Rule 14: It is possible for a Policy to exclude one or more Exclusions.
+exclusion(PolicyID, Cause).
+
+% Rule 15: It is possible for two Exclusions of the same Slip to be different from each other.
+% Rule 14 takes care of this.
+
+% Rule 16: It is possible for two Deductibles of the same Slip to vary from each other.
+pd_deductible(PolicyID, Amount).
+bi_deductible(PolicyID, Days).
+
+% Rule 17: It is possible for two Limits of the same Slip to be distinct from each other.
+% limits_are_distinct(SlipId, LimitId1, LimitId2) :- slip(SlipId), limit(LimitId1), limit(LimitId2), LimitId1 \\= LimitId2.
+sublimit(SlipID, bi, Amount). % bi can have a sublimit. It is expressed in an amount per month.
+sublimit(SlipID, Cause, Amount). % There can also be sublimits for causes of the loss.
+
+
+% Rule 18: It is possible for a Policy to be of type CAR.
+policy_type(PolicyID, car).
+
+% Rule 19: It is possible for a Policy to be of type IAR.
+policy_type(PolicyID, iar).
+
+% Rule 20: It is forbidden for a Policy to appear of type CAR and IAR at the same time.\
+policy_type(PolicyID, car) :- \+ policy_type(PolicyID, iar).
+policy_type(PolicyID, iar) :- \+ policy_type(PolicyID, car).
+
+
+% Rule 21: It is necessary for a Slip to provide Coverage for Property Damage (PD).
+%provides_pd_coverage(SlipId) :- slip(SlipId), coverage(SlipId, pd).
+% I think maybe it should be on the policy? Or both on the policy and the slip, then take the slip if there is a limit?
+provides_coverage(SlipID, pd). % always true
+
+% Rule 22: It is possible for a Slip to provide Coverage for Business Interruption (BI).
+provides_bi_coverage(SlipId) :- slip(SlipId), coverage(SlipId, bi).
+provides_coverage(SlipID, bi) :- provides_coverage(PolicyID, bi), applicable_policy(PolicyID, SlipID).
+
+
+
+% Rule 23: It is possible for a Slip to provide Coverage for Extra Expenses (EE).
+provides_ee_coverage(SlipId) :- slip(SlipId), coverage(SlipId, ee).
+provides_coverage(SlipID, ee) :- provides_coverage(PolicyID, ee), applicable_policy(PolicyID, SlipID).
+
+% Rule 24: If and only if a Policy provides Coverage for BI, it is obligatory that the Policy provides Coverage for EE.
+provides_ee_coverage(PolicyId) :- provides_bi_coverage(PolicyId).
+
+
+% Rule 25: It is permitted for a Policy to be of any type to give Coverage for BI.\
+% provides_bi_coverage(PolicyId) :- policy(PolicyId).\
+% Rule 12 handles this?
+
+% Rule 26: It is necessary for each Slip to share a Share of the Policy.
+share(SlipID, Percent), applicable_policy(PolicyID, SlipID).
+
+% need a loss share number to calculate Acme's share.
+
+% Rule 27: It is obligatory for the Deductible to be either a PD-Deductible or a BI-Deductible.\
+deductible_type(DeductibleId, Type) :- deductible(DeductibleId), member(Type, [pd, bi]).\
+\
+% Rule 28: It is obligatory for the BI-Deductible to be formatted an integer.\
+deductible_format(DeductibleId) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(DeductibleId).\
+\
+% Rule 29: It is obligatory for BI-Deductible to be expressed in number of days.\
+bi_deductible_days(DeductibleId, Days) :- deductible(DeductibleId), deductible_type(DeductibleId, bi), integer(Days).\
+\
+% Rule 30: It is possible for the BI-Deductible to be first 15 days of the Loss.\
+bi_deductible_days(DeductibleId, 15).\
